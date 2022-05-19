68fa2eb8f4c789990091bd99d6c238dba37191c7
Arahanad <arahanad@gmail.com>
2022-05-19 11:53:58 +0530

    databse connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import {app,PORT} from './app'
+import mongoose from 'mongoose';
 
+const DB = "mongodb://localhost:27017/test"
 
-
-
-  app.listen(PORT, ():void => {
-    console.log(`Server Running here 👉 https://localhost:${PORT}`);
+mongoose // mongodb connection with mongoose
+  .connect(DB,{})
+  .then(con => {
+    console.log("db connected successfully");
+  })
+  .catch(err => {
+    console.log("Error connecting to DB" + err);
   });
+
+
+app.listen(PORT, ():void => {
+console.log(`Server Running here 👉 https://localhost:${PORT}`);
+});
